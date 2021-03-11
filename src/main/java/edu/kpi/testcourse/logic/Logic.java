baf63866634fbe96c4ef74cb1e4a0933d3a1d4ab
Dmitri Gribenko <gribozavr@gmail.com>
2021-03-11 15:53:54 +0100

    Converted Logic to a class; there seems to be no need for an interface

--- src/main/java/edu/kpi/testcourse/logic/Logic.java
+++ src/main/java/edu/kpi/testcourse/logic/Logic.java
@@ -1,10 +1,30 @@
 package edu.kpi.testcourse.logic;
 
+import edu.kpi.testcourse.storage.AuthTokenRepository;
+import edu.kpi.testcourse.storage.UrlRepository;
+import edu.kpi.testcourse.storage.UserRepository;
+import javax.inject.Inject;
+
 /**
  * Business logic of the URL shortener application.
  */
-public interface Logic {
+public class Logic {
+  private final UserRepository users;
+  private final AuthTokenRepository tokens;
+  private final UrlRepository urls;
 
-  String hello();
+  /**
+   * Creates an instance.
+   */
+  @Inject
+  public Logic(UserRepository users, AuthTokenRepository tokens, UrlRepository urls) {
+    this.users = users;
+    this.tokens = tokens;
+    this.urls = urls;
+  }
+
+  public String hello() {
+    return "{\"first\": \"Hello\", \"second\": \"world!\"}";
+  }
 
 }
